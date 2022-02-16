eaa01ca26d8c666233bea0d383f0b61da1a743bd
root <biniyamgetaneh30@gmail.com>
2022-02-16 09:08:30 -0800

    114-bst_remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,55 +1,106 @@
 #include "binary_trees.h"
 
 /**
- * bst_remove - removes a node from a Binary Search Tree
- * @root: a pointer to the root node of the tree where you will remove a node
- * @value: the value to remove in the tree
- * Return: a pointer to the new root node of the tree after removal
- *         NULL on failure
+ * bst_search - searches for a value in a Binary Search Tree
+ * @tree: pointer to root of tree
+ * @value: input value
+ * Return: pointer to the node containing a value equals to value
  */
-bst_t *bst_remove(bst_t *root, int value)
+bst_t *bst_search(const bst_t *tree, int value)
 {
-	bst_t *tmp = NULL;
-
-	if (!root)
+	if (!tree)
 		return (NULL);
-
-	if (value < root->n)
-		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
-	else
+	while (tree)
 	{
-		if (!root->left)
-		{
-			tmp = root->right;
-			free(root);
-			return (tmp);
-		}
-		else if (!root->right)
-		{
-			tmp = root->left;
-			free(root);
-			return (tmp);
-		}
-		tmp = bst_min_val(root->right);
-		root->n = tmp->n;
-		root->right = bst_remove(root->right, tmp->n);
+		if (value == tree->n)
+			return ((bst_t *)tree);
+		tree = value < tree->n ? tree->left
+			: tree->right;
 	}
-	return (root);
+	return ((bst_t *)tree);
 }
 
 /**
- * bst_min_val - finds the smallest node from a Binary Search Tree
- * @root: a pointer to the root node of the tree
- * Return: a pointer to the smallest node
+ * swap - swaps two nodes in binary tree
+ * @node: first node
+ * @new: second node
+ * Return: pointer to root
  */
-bst_t *bst_min_val(bst_t *root)
+bst_t *swap(bst_t *node, bst_t *new)
 {
-	bst_t *min = root;
+	bst_t *temp = NULL;
+	_Bool left_child = false;
 
-	while (min->left)
-		min = min->left;
+	if (node->parent)
+		left_child = node->parent->left == node;
+	if (new->parent && new->parent != node)
+		new->parent->left = NULL;
+	new->parent = node->parent;
+	if (node->parent)
+	{
+		if (left_child)
+			node->parent->left = new;
+		else
+			node->parent->right = new;
+	}
+	if (node->left != new)
+	{
+		new->left = node->left;
+		node->left->parent = new;
+	}
+	if (node->right && node->right != new)
+	{
+		new->right = node->right;
+		node->right->parent = new;
+	}
+	temp = new;
+	while (temp->parent)
+		temp = temp->parent;
+	free(node);
+	return (temp);
+}
 
-	return (min);
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to root of tree
+ * @value: input value
+ * Return: pointer to the new root node of the tree after removing the
+ * desired value
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *node, *temp;
+	_Bool left_child = false;
+
+	if (!root)
+		return (NULL);
+	node = bst_search(root, value);
+	if (!node)
+		return (NULL);
+	if (node->parent)
+		left_child = node->parent->left == node;
+	if (!node->right && !node->left)
+	{
+		if (!node->parent)
+		{
+			free(node);
+			return (NULL);
+		}
+		if (left_child)
+			node->parent->left = NULL;
+		else
+			node->parent->right = NULL;
+		temp = node->parent;
+		while (temp->parent)
+			temp = temp->parent;
+		free(node);
+		return (temp);
+
+	}
+	if (!node->right)
+		return (swap(node, node->left));
+	temp = node->right;
+	while (temp->left)
+		temp = temp->left;
+	return (swap(node, temp));
 }
